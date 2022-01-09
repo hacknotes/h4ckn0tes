71cd3a9eab68862917e695d71b3a1c97e5c64f50
Johnny Chafla <91343759+hacknotes@users.noreply.github.com>
2022-01-09 16:15:56 -0500

    Update sistema.sql

--- SQL INJECTION/Error Based/Ejercicio Práctico/sistema.sql
+++ SQL INJECTION/Error Based/Ejercicio Práctico/sistema.sql
@@ -1,5 +1,16 @@
+-- 1. Crear un usuario
+CREATE USER 'sqluser'@'localhost' IDENTIFIED BY 'password123';
+
+-- 2. Asignar permisos al usuario
+GRANT ALL PRIVILEGES ON * . * TO 'sqluser'@'localhost';
+
+-- 3. Cargar los permisos
+FLUSH PRIVILEGES;
+
+-- 4. Crear la Base de Datos sistema
 CREATE DATABASE IF NOT EXISTS sistema;
 
+-- 5. Crear la tabla datos
 CREATE TABLE IF NOT EXISTS datos 
 ( 
     id int(10) not null,
