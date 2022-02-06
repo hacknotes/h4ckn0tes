a59ed57d18ce54e4474cb39b93c8be397a2a238a
Johnny Chafla <91343759+hacknotes@users.noreply.github.com>
2022-02-06 13:56:45 -0500

    Update sistema.sql

--- SQL INJECTION/Error Based/Ejercicio Práctico/sistema.sql
+++ SQL INJECTION/Error Based/Ejercicio Práctico/sistema.sql
@@ -15,7 +15,8 @@ CREATE TABLE IF NOT EXISTS datos
 ( 
     id int(10) not null,
     nombre varchar (25),
-    dirrecion varchar(25),
+	password varchar(25),
+    direccion varchar(25),
     telefono varchar(15),
     PRIMARY KEY (id) 
 ) Engine=InnoDB default charset=latin1; 
